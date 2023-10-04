9cc6086d357479e671b32d72ec69d1bc275c11d2
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-10-04 10:32:43 +0100

    Added a character limit in text area and edited MakeDataArray to provide feedback to the user when the output is being loaded and when they haven't entered a word. Also added some comments documenting the code whih will be built on later

--- src/getData.jsx
+++ src/getData.jsx
@@ -1,7 +1,7 @@
+//given a single word string, and the first item of the "meanings" array returned from a call to the Free Dictionary API, create a "hint" string. The hint string is either the first example sentence with the word itself removed to form a cloze deletion hint, or if no example sentence is available, return the first definition followed by "_____".
 const getHint = (word, meaningsObject) => {
-  //need to edit this function to ensure that the word is still replaced if it begins with a capital letter eg if it is at the start of the example sentence
+
     const pattern = new RegExp(word, "gi")
-    console.log(pattern)
 
     const definition = meaningsObject.definitions.find((item) => item.example)
     if (!definition) {
@@ -11,7 +11,7 @@ const getHint = (word, meaningsObject) => {
   }
   
   const getWordData = async (word) => {
-    //add error handling in case word is not available
+    //given a single word string, call the Free Dictionary API and get the dictionary data for the word. Use getHint() as defined above to create a hint for the word, then return an object containing the word, part of speech and the hint created by the getHint() function in an object literal.
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
 
     try {
@@ -26,7 +26,7 @@ const getHint = (word, meaningsObject) => {
       }
     
       return wordData
-
+    //add error handling in case word is not available
     } catch (error) {
       const wordData = {
         word: word,
@@ -40,9 +40,28 @@ const getHint = (word, meaningsObject) => {
   }
   
   const makeDataArray = async (inputString, setFunction) => {
+    //given an input string from the text entered by the user, remove all punctuation and change to lowercase. Split into separate words and call getWordData() on each word, to create an object with required data for each word. Create an array containing one data object for each word input by the user. Then, call the setFunction (passed in as an arument from App.jsx), which will set this newly-created array as the data to be processed by the table components (TableComponents.jsx)
+    const loadingData = [{
+      word: "loading...",
+      partOfSpeech: "loading...",
+      hint: "*** - loading data. Please wait a moment... - ***"
+    }]
+    setFunction(loadingData)
+
+    const noWordsEntered = [{
+      word: "not provided",
+      partOfSpeech: "-",
+      hint: "*** - Please enter a word or words (separated by spaces or line breaks) in the box above - ***"
+    }]
+
     let cleanString = inputString.replaceAll(/[^\w]/g, ' ').toLowerCase()
     console.log("cleanstring = ", cleanString);
     let inputArray = cleanString.match(/[^\s]+/g)
+
+    if (!inputArray) {
+      setFunction(noWordsEntered)
+    }
+
     let output = []
     for (let i = 0; i < inputArray.length; i++) {
         const data = await getWordData(inputArray[i])
--- src/inputForm.jsx
+++ src/inputForm.jsx
@@ -2,8 +2,8 @@ const InputForm = (props) => {
     return (
       <form className="width-limited">
         <div className="mb-3">
-          <label htmlFor="inputBox" className="form-label">Input your words here</label>
-          <textarea className="form-control" id="inputBox" rows="3"></textarea>
+          <label htmlFor="inputBox" className="form-label">Input your words here (up to 500 characters)</label>
+          <textarea className="form-control" id="inputBox" rows="3" maxlength="500"></textarea>
         </div>
         <button type="button" onClick={props.clickFunction} className="btn btn-primary">Create Flashcards</button>
       </form>
