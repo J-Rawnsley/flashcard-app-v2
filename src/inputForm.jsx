dab96f1ac5d5a828735e9f5977f8c3e49e9dd475
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-10-14 12:28:03 +0100

    testing CI/CD with first auto deployment via git push

--- src/inputForm.jsx
+++ src/inputForm.jsx
@@ -2,7 +2,7 @@ const InputForm = (props) => {
     return (
       <form className="width-limited">
         <div className="mb-3">
-          <label htmlFor="inputBox" className="form-label">Input your words here (up to 500 characters)</label>
+          <label htmlFor="inputBox" className="form-label">Input your words here (up to 500 characters with words separated by lines, spaces or commas)</label>
           <textarea className="form-control" id="inputBox" rows="3" maxlength="500"></textarea>
         </div>
         <button type="button" onClick={props.clickFunction} className="btn btn-primary">Create Flashcards</button>
